110218b205cc17021d362af7bae31ac6977a9af9
Vitalij Vasilev <61069195+orfosit@users.noreply.github.com>
2023-12-07 15:15:49 +0300

    Update docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,7 +3,11 @@
 if [ "$1" = "ragent" ]; then
 
   exec gosu usr1cv8 /opt/1cv8/current/ragent
-  
+  cd /home/usr1cv8/test_baseCD/
+  mkdir ss-data
+  apt-get update
+  apt install wget
+  wget https://disk.yandex.ru/d/jXv3kdalJf47rQ
   cd /opt/1cv8/x86_64/8.3.23.1912/
   ./ibsrv --db-path=/home/usr1cv8/test_baseCD --data=/home/usr1cv8/test_baseCD/ss-data --name=autonomusdb --port=8160 --enable-direct-gate --direct-regport=1570 --http-address=0.0.0.0 
   --debug-address=0.0.0.0 --daemon  --config=/home/usr1cv8/test_baseCD/publication.yml
