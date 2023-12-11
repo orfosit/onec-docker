b1357319dd547449c5bea59a1512f568649759d4
Vitalij Vasilev <61069195+orfosit@users.noreply.github.com>
2023-12-11 13:07:37 +0300

    Update docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$1" = "ragent" ]; then
 fi
 if [ "$1" = "autonomus" ]; then
   cd /opt/1cv8/x86_64/8.3.23.1912
-  ./ibsrv --db-path=/home/usr1cv8/test_baseCD --data=/home/usr1cv8/test_baseCD/ss-data --name=autonomusdb --port=8160 --enable-direct-gate --direct-regport=1570 --http-address=0.0.0.0 --daemon --config=/home/usr1cv8/test_baseCD/publication.yml
+  ./ibsrv --db-path=/home/usr1cv8/test_baseCD --data=/home/usr1cv8/test_baseCD/ss-data --name=autonomusdb --port=8160 --enable-direct-gate --direct-regport=1570 --http-address=0.0.0.0 --daemon --config=/home/usr1cv8/publication.yml
   exit  
 fi 
   
