a1e1a59f9b237630f2c55df873919d76b0aa2783
Vitalij Vasilev <61069195+orfosit@users.noreply.github.com>
2023-12-12 02:09:42 +0300

    Update docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -7,6 +7,7 @@ fi
 if [ "$1" = "autonomus" ]; then
   cd /opt/1cv8/x86_64/8.3.23.1912
   exec ./ibsrv --db-path=/home/usr1cv8/test_baseCD --data=/home/usr1cv8/test_baseCD/ss-data --name=autonomusdb --port=8160 --direct-regport=1570 --http-address=0.0.0.0 --config=/home/usr1cv8/test_baseCD/publication.yml --daemon 
+  exit
 fi 
   
  
