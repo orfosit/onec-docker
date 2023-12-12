b590240551e9ea5a88eda392dd8ee2b69fdbf529
Vitalij Vasilev <61069195+orfosit@users.noreply.github.com>
2023-12-12 12:25:21 +0300

    Update docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 if [ "$1" = "ragent" ]; then
-  exec gosu usr1cv8 /opt/1cv8/current/ragent  
+  exec gosu usr1cv8 /opt/1cv8/current/ragent
   exit
 fi
 if [ "$1" = "autonomus" ]; then
   cd /opt/1cv8/x86_64/8.3.23.1912
-  exec ./ibsrv --db-path=/home/usr1cv8/test_baseCD --data=/home/usr1cv8/test_baseCD/ss-data --name=autonomusdb --port=8160 --direct-regport=1570 --http-address=0.0.0.0 --config=/home/usr1cv8/test_baseCD/publication.yml --daemon 
+  exec ./ibsrv --db-path=/home/usr1cv8/test_baseCD --daemon --data=/home/usr1cv8/test_baseCD/ss-data --name=autonomusdb --port=8160 --direct-regport=1570 --http-address=0.0.0.0 --config=/home/usr1cv8/test_baseCD/publication.yml
   exit
-fi 
-  
+fi
+
+
+
+
  
